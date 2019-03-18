daadfac2953fecb2c58b184730d4693574550221
Daniel Berlin <dberlin@dberlin.org>
2019-03-17 21:09:52 -0700

    Make these protected

--- FileTries.ts
+++ FileTries.ts
@@ -2,8 +2,8 @@ import { Trie } from "tiny-trie";
 import { SymbolInfo } from "../SymbolInfo";
 import { normalizeSymbolName } from "../util";
 export class BaseFileTries {
-  private allSymbols: Trie = new Trie();
-  private symbolMap: Map<string, SymbolInfo> = new Map();
+  protected allSymbols: Trie = new Trie();
+  protected symbolMap: Map<string, SymbolInfo> = new Map();
   /**
    * Return all symbols that are valid completions of prefix.
    *
